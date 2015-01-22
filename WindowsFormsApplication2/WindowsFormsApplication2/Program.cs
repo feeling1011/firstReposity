402bc929aef6c00e11ca3c67930f65006a250dfb
Lena.Li <Lena.Li@finisar.com>
2015-01-22 14:25:06 +0800

    test

--- WindowsFormsApplication2/WindowsFormsApplication2/Program.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Program.cs
@@ -17,5 +17,8 @@ namespace WindowsFormsApplication2
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+        public void test01() { 
+        }
     }
 }
